8f6ee706d8ae21dcd3d0409261747c2c3407ba50
ameljohnycd <developerat01@gmail.com>
2017-12-14 20:11:47 +0530

    Login and registration working withfirebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+const firebaseAuth= {
+    apiKey: "AIzaSyAwKSj0ER8zsXXmK1pDDpEaqsEzm7bxWPQ",
+    authDomain: "virtualdeck-01.firebaseapp.com",
+    databaseURL: "https://virtualdeck-01.firebaseio.com",
+    projectId: "virtualdeck-01",
+    storageBucket: "virtualdeck-01.appspot.com",
+    messagingSenderId: "267200803892"
+  };
+  
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    LoginPage,
+    RegisterPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseAuth),
+    AngularFireAuthModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    LoginPage,
+    RegisterPage
   ],
   providers: [
     StatusBar,
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,25 +1,31 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { AngularFireAuth } from 'angularfire2/auth';
 
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-login',
-  templateUrl: 'login.html',
+  templateUrl: 'login.html'
 })
 export class LoginPage {
+  @ViewChild('username') username;
+  @ViewChild('password') password;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private fire: AngularFireAuth,public navCtrl: NavController) {
+    
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+  SignInUser(){
+  	this.fire.auth.signInWithEmailAndPassword(this.username.value,this.password.value)
+  	.then(() => {
+  		console.log('userloggedIN');
+
+  		})
+  	.catch( error => {
+  		console.log('got error', error);
+  		
+  		}) 
+   
+    
   }
 
 }
